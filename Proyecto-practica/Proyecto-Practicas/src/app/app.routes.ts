979e5b64daae61f9504ee778c5e671ec7c42c3be
Daniel Agudelo <dalfonsoagudelo@ucompensar.edu.co>
2024-04-26 02:26:14 +0000

    cambios de la clase

--- Proyecto-practica/Proyecto-Practicas/src/app/app.routes.ts
+++ Proyecto-practica/Proyecto-Practicas/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { AddPersonaComponent } from './componentes/persona/add-persona/add-persona.component';
+import { EditPersonaComponent } from './componentes/persona/edit-persona/edit-persona.component';
+import { DeletePersonaComponent } from './componentes/persona/delete-persona/delete-persona.component';
+import { ListaPersonaComponent } from './componentes/persona/lista-persona/lista-persona.component';
+
+export const routes: Routes = [
+    {path:"add", component: AddPersonaComponent},
+    {path:"edit/:id", component: EditPersonaComponent},
+    {path:"delete/:id", component: DeletePersonaComponent},
+    {path:"", component: ListaPersonaComponent},
+    {path:"**", redirectTo:"", pathMatch:"full"}
+];
